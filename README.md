ce0dcc228042cf7cb93a85120a51419c855a88e1
Joe Fatora <joe@willing.com>
2017-09-17 17:18:35 -0400

    added space switching with shortcut

--- README.md
+++ README.md
@@ -88,8 +88,10 @@ Use <kbd>control</kbd> + <kbd>s</kbd> to turn on Window Layout Mode. Then, use a
 - Use <kbd>space</kbd> to send window to center of screen
 - Use <kbd>enter</kbd> to resize window to fill the screen
 - Use <kbd>n</kbd> to send window to the next monitor
-- Use <kbd>←</kbd> to send window to the monitor on the left (if there is one)
-- Use <kbd>→</kbd> to send window to the monitor on the right (if there is one)
+- Use <kbd>←</kbd> to send window to the space on the left (if there is one)
+- Use <kbd>→</kbd> to send window to the space on the right (if there is one)
+- Use <kbd>shift + ←</kbd> to send window to the monitor on the left (if there is one)
+- Use <kbd>shift + →</kbd> to send window to the monitor on the right (if there is one)
 - Use <kbd>control</kbd> + <kbd>s</kbd> to exit Window Layout Mode without moving any windows
 
 [<img src="https://cloud.githubusercontent.com/assets/2988/22397114/715cc12e-e538-11e6-9dcd-b3447af0d9dd.png" alt="Window Layout Mode Keybindings (1)" width="400"/>](https://cloud.githubusercontent.com/assets/2988/22397114/715cc12e-e538-11e6-9dcd-b3447af0d9dd.png) [<img src="https://cloud.githubusercontent.com/assets/2988/22397111/45672fe6-e538-11e6-905d-5b0234e290bb.png" alt="Window Layout Mode Keybindings (2)" width="400"/>](https://cloud.githubusercontent.com/assets/2988/22397111/45672fe6-e538-11e6-905d-5b0234e290bb.png)
@@ -161,6 +163,7 @@ This setup is honed and tested with the following dependencies.
 - macOS Sierra, 10.12
 - [Karabiner-Elements 0.91.7][karabiner]
 - [Hammerspoon 0.9.52][hammerspoon]
+- [Hammerspoon undocumented spaces extention](https://github.com/asmagill/hs._asm.undocumented.spaces)
 
 ## Installation
 
--- hammerspoon/windows.lua
+++ hammerspoon/windows.lua
@@ -1,4 +1,115 @@
 hs.window.animationDuration = 0
+local spaces           = require('hs._asm.undocumented.spaces')
+
+local cache = {
+  mousePosition = nil
+}
+
+-- grabs screen with active window, unless it's Finder's desktop
+-- then we use mouse position
+local function activeScreen()
+  local mousePoint = hs.geometry.point(hs.mouse.getAbsolutePosition())
+  local activeWindow = hs.window.focusedWindow()
+
+  if activeWindow and activeWindow:role() ~= 'AXScrollArea' then
+    return activeWindow:screen()
+  else
+    return hs.fnutils.find(hs.screen.allScreens(), function(screen)
+        return mousePoint:inside(screen:frame())
+      end)
+  end
+end
+
+local function focusScreen(screen)
+  local frame = screen:frame()
+
+  -- if mouse is already on the given screen we can safely return
+  if hs.geometry(hs.mouse.getAbsolutePosition()):inside(frame) then return false end
+
+  -- "hide" cursor in the lower right side of screen
+  -- it's invisible while we are changing spaces
+  local mousePosition = {
+    x = frame.x + frame.w - 1,
+    y = frame.y + frame.h - 1
+  }
+
+  -- hs.mouse.setAbsolutePosition doesn't work for gaining proper screen focus
+  -- moving the mouse pointer with cliclick (available on homebrew) works
+  os.execute(template([[ /usr/local/bin/cliclick m:={X},{Y} ]], { X = mousePosition.x, Y = mousePosition.y }))
+  hs.timer.usleep(1000)
+
+  return true
+end
+
+local function activeSpaceIndex(screenSpaces)
+  return hs.fnutils.indexOf(screenSpaces, spaces.activeSpace()) or 1
+end
+
+local function screenSpaces(currentScreen)
+  currentScreen = currentScreen or activeScreen()
+  return spaces.layout()[currentScreen:spacesUUID()]
+end
+
+local function spaceInDirection(direction)
+  local screenSpaces = screenSpaces()
+  local activeIdx = activeSpaceIndex(screenSpaces)
+  local targetIdx = direction == 'left' and activeIdx - 1 or activeIdx + 1
+
+  return screenSpaces[targetIdx]
+end
+
+local function moveWindowOneSpace(win, direction)
+  local clickPoint = win:zoomButtonRect()
+  local sleepTime = 1000
+  local targetSpace = spaceInDirection(direction)
+
+  -- check if all conditions are ok to move the window
+  local shouldMoveWindow = hs.fnutils.every({
+      clickPoint ~= nil,
+      targetSpace ~= nil,
+      not cache.movingWindowToSpace
+    }, function(test) return test end)
+
+  if not shouldMoveWindow then return end
+
+  cache.movingWindowToSpace = true
+
+  cache.mousePosition = cache.mousePosition or hs.mouse.getAbsolutePosition()
+
+  clickPoint.x = clickPoint.x + clickPoint.w + 5
+  clickPoint.y = clickPoint.y + clickPoint.h / 2
+
+  -- fix for Chrome UI
+  if win:application():title() == 'Google Chrome' then
+    clickPoint.y = clickPoint.y - clickPoint.h
+  end
+
+  -- focus screen before switching window
+  focusScreen(win:screen())
+
+  hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftMouseDown, clickPoint):post()
+  hs.timer.usleep(sleepTime)
+
+  hs.eventtap.keyStroke({ 'ctrl' }, direction)
+
+  hs.timer.waitUntil(
+    function()
+      return spaces.activeSpace() == targetSpace
+    end,
+    function()
+      hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftMouseUp, clickPoint):post()
+
+      -- resetting mouse after small timeout is needed for focusing screen to work properly
+      hs.mouse.setAbsolutePosition(cache.mousePosition)
+      cache.mousePosition = nil
+
+      -- reset cache
+      cache.movingWindowToSpace = false
+
+    end,
+    0.01 -- check every 1/100 of a second
+  )
+end
 
 -- +-----------------+
 -- |        |        |
@@ -176,6 +287,7 @@ end
 -- | HERE      |     |
 -- |           |     |
 -- +-----------------+
+-- add 5 to cover annoying spaces
 function hs.window.left66(win)
   local f = win:frame()
   local screen = win:screen()
@@ -183,7 +295,7 @@ function hs.window.left66(win)
 
   f.x = max.x
   f.y = max.y
-  f.w = max.w * (2/3)
+  f.w = max.w * (2/3) + 5
   f.h = max.h
   win:setFrame(f)
 end
@@ -193,12 +305,13 @@ end
 -- |           | HERE|
 -- |           |     |
 -- +-----------------+
+-- add 5 to cover annoying spaces
 function hs.window.right33(win)
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x + (max.w/3)
+  f.x = max.x + (max.w * 2/3) + 5
   f.y = max.y
   f.w = max.w * (1/3)
   f.h = max.h
@@ -233,6 +346,14 @@ function hs.window.nextScreen(win)
   else
     win:moveToScreen(allScreens[1])
   end
+end
+
+function hs.window.moveWindowOneSpaceRight(win)
+  moveWindowOneSpace(win, 'right')
+end
+
+function hs.window.moveWindowOneSpaceLeft(win)
+  moveWindowOneSpace(win, 'left')
 end
 
 --------------------------------------------------------------------------------
@@ -327,13 +448,20 @@ windowLayoutMode:bindWithAutomaticExit({}, 'n', function()
 end)
 
 windowLayoutMode:bindWithAutomaticExit({}, 'right', function()
-  hs.window.focusedWindow():moveOneScreenEast()
+  hs.window.focusedWindow():moveWindowOneSpaceRight()
 end)
 
 windowLayoutMode:bindWithAutomaticExit({}, 'left', function()
-  hs.window.focusedWindow():moveOneScreenWest()
+  hs.window.focusedWindow():moveWindowOneSpaceLeft()
 end)
 
+windowLayoutMode:bindWithAutomaticExit({'shift'}, 'right', function()
+  hs.window.focusedWindow():moveOneScreenEast()
+end)
+
+windowLayoutMode:bindWithAutomaticExit({'shift'}, 'left', function()
+  hs.window.focusedWindow():moveOneScreenWest()
+end)
 -- Use Control+s to toggle WindowLayout Mode
 hs.hotkey.bind({'ctrl'}, 's', function()
   windowLayoutMode:enter()
